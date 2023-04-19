56e2f640423aa31c5f5408b24fbd5978c9d10192
RTourt <revengeofnoraneko@gmail.com>
2023-04-19 18:16:48 +0200

    💄 F03: Login > LoginForm > add styling

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -21,7 +21,7 @@ export default function LoginForm() {
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
-      <br />
+      <hr />
       <h2>Connectez-vous</h2>
       <input
         value={inputValue}
@@ -37,4 +37,28 @@ export default function LoginForm() {
 
 const LoginFormStyled = styled.form`
   background: green;
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0 auto;
+  padding: 2.5rem 2rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
+
+  h1 {
+    color: white;
+    font-size: 48px;
+  }
+
+  hr {
+    border: 1px solid #f56a2c;
+    margin-bottom: 40px;
+  }
+
+  h2 {
+    color: #8e8b8b;
+    margin: 20px 10px 10px;
+    color: white;
+    font-size: 36px;
+  }
 `;
