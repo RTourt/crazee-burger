7759a2b608c8fee7d2c294e78588c4555468271f
RTourt <revengeofnoraneko@gmail.com>
2023-04-20 17:50:16 +0200

    Login > LoginForm > Replace hard coded value with Design System values

--- src/components/reusable-ui/PrimaryButton.js
+++ src/components/reusable-ui/PrimaryButton.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { theme } from "../../theme";
 export default function PrimaryButton({ label, Icon }) {
   return (
     <PrimaryButtonStyled>
@@ -10,7 +11,6 @@ export default function PrimaryButton({ label, Icon }) {
 
 const PrimaryButtonStyled = styled.button`
   width: 100%;
-  border: 1px solid red;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -23,21 +23,21 @@ const PrimaryButtonStyled = styled.button`
   border-radius: 5px;
   font-size: 15px;
   font-weight: 800;
-  color: white;
-  background-color: #ff9f1b;
-  border: 1px solid #ff9f1b;
+  color: ${theme.colors.white};
+  background-color: ${theme.colors.primary};
+  border: 1px solid ${theme.colors.primary};
 
   &:hover:not(:disabled) {
-    background-color: white;
-    color: #ff9f1b;
-    border: 1px solid #ff9f1b;
+    background-color: ${theme.colors.white};
+    color: ${theme.colors.primary};
+    border: 1px solid ${theme.colors.primary};
     transition: all 200ms ease-out;
   }
 
   &:active {
-    color: white;
-    background-color: #ff9f1b;
-    border: 1px solid #ff9f1b;
+    color: ${theme.colors.white};
+    background-color: ${theme.colors.primary};
+    border: 1px solid ${theme.colors.primary};
   }
 
   &:disabled {
