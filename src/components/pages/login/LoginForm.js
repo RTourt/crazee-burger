4b7bd26f18b325834966efc2d84c87fc8e907b39
RTourt <revengeofnoraneko@gmail.com>
2023-04-20 15:20:52 +0200

    ♻️ F03: Login > LoginForm > Create reusable <Input/>

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { BsPersonCircle } from "react-icons/bs";
 import { IoChevronForward } from "react-icons/io5";
+import Input from "./Input";
 // import { theme } from "../../../theme";
 
 export default function LoginForm() {
@@ -29,16 +30,13 @@ export default function LoginForm() {
       </div>
 
       <div>
-        <div className="input-with-icon">
-          <BsPersonCircle className="icon" />
-          <input
-            value={inputValue}
-            onChange={handleChange}
-            type="text"
-            placeholder="Entrez votre prénom"
-            required
-          />
-        </div>
+        <Input
+          value={inputValue}
+          onChange={handleChange}
+          placeholder={"Entrez votre prénom"}
+          required
+          Icon={<BsPersonCircle className="icon" />}
+        />
       </div>
 
       <button className="button-with-icon">
@@ -73,33 +71,6 @@ const LoginFormStyled = styled.form`
     margin: 20px 10px 10px;
     color: white;
     font-size: 36px;
-  }
-
-  .input-with-icon {
-    background-color: #fff;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    padding: 12px 24px;
-    margin: 18px 0;
-
-    .icon {
-      font-size: 15px;
-      margin-right: 8px;
-      color: #93a2b1;
-    }
-
-    input {
-      border: none;
-      font-size: 15px;
-      color: #17161a;
-      width: 100%;
-    }
-
-    &::placeholder {
-      background: white;
-      color: lightgrey;
-    }
   }
 
   .button-with-icon {
